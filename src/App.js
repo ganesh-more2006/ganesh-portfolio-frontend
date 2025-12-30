ae61467e2cd2644370b1824d20f1cb903a067fa8
ganesh-more2006 <moreganesh855197@gmail.com>
2025-12-30 14:08:26 +0530

    Folders restored and submodule fixed

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    
     const fetchProjects = async () => {
       try {
         console.log("System: Starting data fetch from MongoDB...");
